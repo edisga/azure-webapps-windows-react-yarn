91a10ec3bcde72abda3eb910dc55ba748bc6ff5d
Edison Garcia <egachi@live.com>
2019-11-25 20:31:40 -0600

    Updating React Deployment with Yarn

--- deploy.cmd
+++ deploy.cmd
@@ -88,28 +88,38 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-:: 2. Select node version
+:: 1. Select node version
 call :SelectNodeVersion
 
-:: 3. Install Yarn
+:: 2. Install Yarn
 echo Verifying Yarn Install.
 call :ExecuteCmd !NPM_CMD! install yarn -g
 
-:: 4. Install Yarn packages
+:: 3. Install Yarn packages
 echo Installing Yarn Packages.
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd yarn install --production
   call :ExecuteCmd yarn build 
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+
+:: 4. Building Yarn site
+echo Building Yarn site.
+IF EXIST "%DEPLOYMENT_SOURCE%\node_modules" (
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd yarn build 
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 5. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/build" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
